35665452d06b837e71b48d6b9b2809de8c8440ec
Aravind Sekar <aravind.s@contus.in>
2021-02-21 10:19:59 +0530

    Initial Commit

---
+++ GitHubSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubSearch
+//
+//  Created by Aravind Sekar on 21/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
